1c4c6873f3c859dbd8d220777d2e135a2000f4f8
Gustavo Daitx <110948095+gustavodaitx@users.noreply.github.com>
2024-11-27 21:42:18 -0300

    Added project files

--- Carro/resources/views/carros/edit.blade.php
+++ Carro/resources/views/carros/edit.blade.php
@@ -1,38 +1,8 @@
-@extends('layout.base');
-
-@section('content');
-
-
-
-
-<form class="max-w-sm mx-auto" action="{{ url('carros/'.$carro->id) }}"  method="POST" enctype="multipart/form-data">
-
+<form action="{{ url('carros/'.$carros->id) }}" method="POST">
     @csrf
-    @method ('PUT')
-    
-  <div class="mb-5">
-      <label for="nome" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome</label>
-      <input type="text" name="nome" id="nome" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"required>
-  </div>
- 
-  <div class="mb-5">
-      <label for="modelo" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Modelo</label>
-      <input type="text" name ="modelo" id="type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"required>
-  </div>
-
-  <div class="mb-5">
-      <label for="ano" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ano</label>
-      <input type="number" name="ano" id="power" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"required>
-  </div>
-
-  <div>
-    <label for="image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image</label>
-    <input type="file" name="image" id="image" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-  </div>
-
-  <br>
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" type="submit">Create Car</button> 
-
+    @method('PUT')
+    <input type="text" name="nome" placeholder="Nome" required>
+    <input type="text" name="modelo" placeholder="modelo" required>
+    <input type="numeric" name="ano" placeholder="ano" required>
+    <button type="submit">Editar Product</button>
 </form>
-
-@endsection
