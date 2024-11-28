80869ba1ffdf3a3d842ddf0c7b898cf4945389b5
Gustavo Daitx <110948095+gustavodaitx@users.noreply.github.com>
2024-11-27 21:52:36 -0300

    Added project files

--- Carro/routes/web.php
+++ Carro/routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
-use App\Http\Controllers\CarroController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('carros', [CarroController::class, 'index']);
-Route::get('carros/create', [CarroController::class, 'create']);
-Route::post('carros', [CarroController::class, 'store']);
-Route::get('carros/{id}/edit', [CarroController::class, 'edit']);
-Route::put('carros/{id}', [CarroController::class, 'update']);
-Route::delete('carros/{id}', [CarroController::class, 'destroy']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
