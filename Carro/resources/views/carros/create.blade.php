f0719b771d3de617267a3e2326f7124e91130ece
Gustavo Daitx <110948095+gustavodaitx@users.noreply.github.com>
2024-11-27 21:26:24 -0300

    Added project files

--- Carro/resources/views/carros/create.blade.php
+++ Carro/resources/views/carros/create.blade.php
@@ -1,22 +1,21 @@
 @extends('layout.base')
 
-@section('content')
 
 <form class="max-w-sm mx-auto" action="{{ url('carros') }}" method="POST" enctype="multipart/form-data">
  
   @csrf 
-  <div class="mb-5">
-      <label for="nome" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome</label>
+  <div >
+      <label for="nome" >Nome</label>
       <input type="text" name="nome" id="nome" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"required>
   </div>
  
-  <div class="mb-5">
-      <label for="modelo" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Modelo</label>
+  <div >
+      <label for="modelo" >Modelo</label>
       <input type="text" name ="modelo" id="type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"required>
   </div>
 
-  <div class="mb-5">
-      <label for="ano" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ano</label>
+  <div >
+      <label for="ano" >Ano</label>
       <input type="number" name="ano" id="power" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"required>
   </div>
 
@@ -26,9 +25,8 @@
   </div>
 
   <br>
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" type="submit">Create Car</button> 
 
-</form>
+  <button  type="submit">Create Car</button> 
 
 
 
--- Carro/resources/views/carros/index.blade.php
+++ Carro/resources/views/carros/index.blade.php
@@ -1,16 +1,14 @@
-@extends('layout.base')
 
-@section('content')
 
-<div class="flex items-start">
-    <div class="py-8 mb-5 p-4">
+<div >
+    <div >
         <a href="{{url('carros/create')}}" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Add Pokemon </a>
     </div>
 
 </div>
 
 <div class="flex flex-wrap justify-center mt-10">
-    @foreach($carro as $entity)
+    @foreach($carros as $entity)
         <div class="p-4 max-w-sm">
             <div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col">
                 <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset($entity->image) }}" alt="{{$entity->name}}"/>
